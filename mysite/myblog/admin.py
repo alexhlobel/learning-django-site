b76526214a439cefba9eed74bf773cf41b749e1a
Alexandr_Pankratov <pasaladr@gmail.com>
2022-05-01 23:24:37 +0300

    Initial commit

---
+++ mysite/myblog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'url': ('title',)}
+    pass
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'post', 'text', 'created_date')
+    list_filter = ('username', 'created_date')
+    search_fields = ['text', 'username__username', 'post__title']
+
+
+admin.site.register(Comment, CommentAdmin)
